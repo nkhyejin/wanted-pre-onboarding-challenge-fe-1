5302af02a677cf084ff657d5f10a451e301d8095
남궁혜진 <nkhj0404@gmail.com>
2023-01-30 07:15:33 +0900

    feat: 스타일 없는 버튼 추가

--- front/src/components/common/button.tsx
+++ front/src/components/common/button.tsx
@@ -6,6 +6,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   width?: string;
   height?: string;
+  textColor?: string;
 }
 
 export const Button = ({ children, width, height, ...props }: ButtonProps) => {
@@ -15,3 +16,33 @@ export const Button = ({ children, width, height, ...props }: ButtonProps) => {
     </button>
   );
 };
+
+export const NoneStyleButton = ({
+  children,
+  width,
+  height,
+  textColor,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button css={{ width, height, textColor, ...noneStyle }} {...props}>
+      {children}
+    </button>
+  );
+};
+
+const noneStyle = css`
+  border: none;
+  padding: 5px;
+  box-shadow: none;
+  border-radius: 0px;
+  background-color: white;
+
+  font-size: 14px;
+
+  &:hover {
+    background-color: white;
+    opacity: 0.5;
+    cursor: pointer;
+  }
+`;
