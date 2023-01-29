c4f15d0072b29cd88e3577ebc0f81c2cf20c4dcc
남궁혜진 <nkhj0404@gmail.com>
2023-01-30 07:36:03 +0900

    fix: button css->styled 변경

--- front/src/components/common/button.tsx
+++ front/src/components/common/button.tsx
@@ -1,43 +1,29 @@
 /** @jsxImportSource @emotion/react */
 import React, { ButtonHTMLAttributes } from 'react';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+interface ButtonProps {
   width?: string;
   height?: string;
   textColor?: string;
 }
 
-export const Button = ({ children, width, height, ...props }: ButtonProps) => {
-  return (
-    <button css={{ width, height }} {...props}>
-      {children}
-    </button>
-  );
-};
+export const Button = styled.button<ButtonProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
 
-export const NoneStyleButton = ({
-  children,
-  width,
-  height,
-  textColor,
-  ...props
-}: ButtonProps) => {
-  return (
-    <button css={{ width, height, textColor, ...noneStyle }} {...props}>
-      {children}
-    </button>
-  );
-};
+export const NoneStyleButton = styled.button<ButtonProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  color: ${(props) => props.textColor};
 
-const noneStyle = css`
   border: none;
   padding: 5px;
   box-shadow: none;
   border-radius: 0px;
   background-color: white;
-
   font-size: 14px;
 
   &:hover {
