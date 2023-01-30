4673a5c1366d0d64857e889a1da16d54242fa369
남궁혜진 <nkhj0404@gmail.com>
2023-01-30 11:53:04 +0900

    fix: navbar todo 페이지연결

--- front/src/components/header/navbar.tsx
+++ front/src/components/header/navbar.tsx
@@ -1,13 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const menus = ['todo', 'mypage'];
-
+  const navigation = useNavigate();
   return (
     <ul css={ulStyle}>
       {menus.map((menu) => (
-        <li key={menu}>{menu}</li>
+        <li key={menu} onClick={() => navigation(`/${menu}`)}>
+          {menu}
+        </li>
       ))}
     </ul>
   );
