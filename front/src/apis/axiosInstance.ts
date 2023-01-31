8939e6d3e46db1dd921723b2d8ef811f8d26bde4
남궁혜진 <nkhj0404@gmail.com>
2023-01-31 17:18:25 +0900

    feat: axios 설정

--- front/src/apis/axiosInstance.ts
+++ front/src/apis/axiosInstance.ts
@@ -1,1 +1,44 @@
-export {};
+import axios from 'axios';
+
+const BASE_URL = 'https://localhost:8080';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 1000,
+  headers: { 'X-Custom-Header': 'foobar' },
+});
+
+// 요청 인터셉터 추가하기
+axiosInstance.interceptors.request.use(
+  function (config) {
+    // 요청이 전달되기 전에 작업 수행
+    // 넘겨줄 데이터 타입에 따라서 다르게 전달한다.
+    if (config.data instanceof FormData) {
+      config.headers['Content-Type'] = 'multipart/form-data';
+    } else {
+      config.headers['Content-Type'] = 'application/json';
+    }
+    config.headers.Authorization = `Bearer ${localStorage.getItem(
+      'userToken',
+    )}`;
+    return config;
+  },
+  function (error) {
+    // 요청 오류가 있는 작업 수행
+    return Promise.reject(error);
+  },
+);
+
+// 응답 인터셉터 추가하기
+axiosInstance.interceptors.response.use(
+  function (response) {
+    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 데이터가 있는 작업 수행
+    return response;
+  },
+  function (error) {
+    // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
+    // 응답 오류가 있는 작업 수행
+    return Promise.reject(error);
+  },
+);
