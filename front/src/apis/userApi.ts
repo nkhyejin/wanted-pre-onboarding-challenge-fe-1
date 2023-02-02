7f59dc48e89c0f8714b11fb708a32eca4d3c0b00
남궁혜진 <nkhj0404@gmail.com>
2023-02-02 14:37:05 +0900

    refac: auth post 요청 하나로 통일

--- front/src/apis/userApi.ts
+++ front/src/apis/userApi.ts
@@ -2,11 +2,18 @@ import axios, { AxiosError } from 'axios';
 import { RegisterInfo, ResponseType, UserInfo } from '../types/user';
 import { axiosInstance } from './axiosInstance';
 
-export const requestLogin = async (info: UserInfo) => {
+const END_POINT = {
+  login: '/users/login',
+  signIn: '/users/create',
+};
+
+//공통 post 요청
+const post = async <T>(info: T, endpoint: string) => {
   try {
-    const { status, data } = await axiosInstance.post('/users/login', info);
+    const { status, data } = await axiosInstance.post(endpoint, info);
     console.log(status, data);
-    localStorage.setItem('userToken', data.token);
+    if (endpoint === END_POINT.login)
+      localStorage.setItem('userToken', data.token);
     return {
       status,
       message: data.message,
@@ -22,20 +29,9 @@ export const requestLogin = async (info: UserInfo) => {
   }
 };
 
-export const requestRegister = async (info: RegisterInfo) => {
-  try {
-    const { status, data } = await axiosInstance.post('/users/create', info);
-    return {
-      status,
-      message: data.message,
-    } as ResponseType;
-  } catch (err) {
-    if (axios.isAxiosError(err) && err.response) {
-      const { status, data } = err.response;
-      return {
-        status,
-        message: data.details,
-      } as ResponseType;
-    }
-  }
+export const requestLogin = (info: UserInfo) => {
+  return post<UserInfo>(info, END_POINT.login);
+};
+export const requestRegister = (info: RegisterInfo) => {
+  return post<RegisterInfo>(info, END_POINT.signIn);
 };
